d7b4d35e30e7521c4585ec9287c0232fbb0f220c
maksymcherniienko <146116469+maksymcherniienko@users.noreply.github.com>
2026-03-23 15:02:12 +0200

    BMP280 sensor initialization and reading I2C

--- lab2ex2.cpp
+++ lab2ex2.cpp
@@ -1,45 +1,37 @@
-#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
-// Визначаємо піни CS (Chip Select)
-#define BMP_CS1 5
-#define BMP_CS2 17
-
-// Створюємо два об'єкти для кожного датчика через SPI
-Adafruit_BMP280 bmp1(BMP_CS1);
-Adafruit_BMP280 bmp2(BMP_CS2);
+Adafruit_BMP280 bmp1; 
+Adafruit_BMP280 bmp2;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Тестування двох BMP280 через SPI...");
+  Serial.println("Пошук двох датчиків BMP280...");
 
-  // Ініціалізація 1-го датчика
-  if (!bmp1.begin()) {
-    Serial.println("Датчик #1 не знайдено! Перевірте пін CS 5");
-    while (1);
+  // Ініціалізація першого датчика (адреса за замовчуванням 0x76)
+  if (!bmp1.begin(0x76)) {
+    Serial.println("Датчик #1 (0x76) не знайдено!");
   }
 
-  // Ініціалізація 2-го датчика
-  if (!bmp2.begin()) {
-    Serial.println("Датчик #2 не знайдено! Перевірте пін CS 17");
-    while (1);
+  // Ініціалізація другого датчика (адреса 0x77)
+  if (!bmp2.begin(0x77)) {
+    Serial.println("Датчик #2 (0x77) не знайдено!");
   }
-
-  Serial.println("Обидва датчики готові!");
 }
 
 void loop() {
-  // Датчик 1
+  // Зчитування з першого датчика
   Serial.print("Датчик 1: T=");
   Serial.print(bmp1.readTemperature());
-  Serial.print("C, P=");
+  Serial.print(" C, P=");
   Serial.print(bmp1.readPressure() * 0.00750062); // в мм рт. ст.
   Serial.println(" mmHg");
 
-  // Датчик 2
+  // Зчитування з другого датчика
   Serial.print("Датчик 2: T=");
   Serial.print(bmp2.readTemperature());
-  Serial.print("C, P=");
+  Serial.print(" C, P=");
   Serial.print(bmp2.readPressure() * 0.00750062);
   Serial.println(" mmHg");
 
